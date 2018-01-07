309a99d2ef317c505e865ffa0fa993d560d1c2f9
Krzysztof Milek <krzysztof.milek73@gmail.com>
2018-01-07 18:16:38 +0100

    Add app logic

---
+++ js/script.js
@@ -0,0 +1,22 @@
+//VARIABLE
+var url = 'http://api.icndb.com/jokes/random';
+var button = document.getElementById('get-joke');
+var paragraph = document.getElementById('joke');
+
+//EVENT
+button.addEventListener('click', function(){
+  getJoke();
+});
+
+window.onload = getJoke;
+
+//FUNCTION
+function getJoke() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.addEventListener('load', function(){
+      var response = JSON.parse(xhr.response);
+      paragraph.innerHTML = response.value.joke;
+    });
+    xhr.send();
+  };
